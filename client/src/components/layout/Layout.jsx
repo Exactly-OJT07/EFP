8ea255dbb7cfbc148c5351941807a7286f1559ae
Le Sy Thanh <lesythanh@Les-MacBook-Pro.local>
2023-11-30 17:29:29 +0700

    demo

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { Col, Layout, Row, Spin, theme } from "antd";
+import { Suspense, createElement, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { LayoutSider } from "./LayoutSider";
 
-const Layout = () => {
+const { Content, Header } = Layout;
+
+export const PrivateLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <div>Layout</div>
-  )
-}
-
-export default Layout
+    <Layout
+      style={{
+        overflow: "hidden",
+        height: "100vh",
+        padding: 0,
+      }}
+    >
+      <LayoutSider collapsed={collapsed} />
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Row align="middle">
+            <Col span={12}>
+              {createElement(
+                collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                {
+                  className: "trigger",
+                  onClick: () => setCollapsed(!collapsed),
+                }
+              )}
+            </Col>
+            <Col span={12}>{/* <DropProfile /> */}</Col>
+          </Row>
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <Suspense fallback={<Spin />}>
+            <Outlet />
+          </Suspense>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { Spin } from "antd";
+import "./index.css";
+import router from "./router";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Suspense fallback={<Spin />}>
+    <RouterProvider router={router} />
+  </Suspense>
+);
