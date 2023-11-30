b60d75fec45ad1e3c10b5e2ebc5e46f899676712
ThangDevP <thang.nguyen-ojt07@devplus.edu.vn>
2023-11-30 13:46:27 +0700

    feat/EFP-3: Create codebase, .env, docker,husky file (BE)

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -15,11 +15,15 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'sonar-scanner.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', vars: 'all', args: 'after-used' },
+    ],
   },
 };
